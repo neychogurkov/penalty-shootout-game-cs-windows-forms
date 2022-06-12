a37760f50b67c5a6350cc031e9ff48dd3dee1b78
Neycho Gurkov <nglolplayer@gmail.com>
2022-06-12 13:43:53 +0300

    Implemented goalkeeper animation

--- PenaltyShootoutGame/Form1.cs
+++ PenaltyShootoutGame/Form1.cs
@@ -7,12 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using PenaltyShootoutGame.Properties;
 
 namespace PenaltyShootoutGame
 {
     public partial class Form1 : Form
     {
         object selectedTarget;
+        string goalkeeperTarget = string.Empty;
         public Form1()
         {
             InitializeComponent();
@@ -57,6 +59,89 @@ namespace PenaltyShootoutGame
             // top right save location:    438, 151
             // top left save location:     421, 155 
             // ball location:              449, 487
+
+            switch (goalkeeperTarget)
+            {
+                case "topleft":
+                    {
+                        goalkeeper.Image = Resources.top_left_save;
+                        if (goalkeeper.Left > topLeft.Left && goalkeeper.Top > topLeft.Top)
+                        {
+                            goalkeeper.Left -= 20;
+                            goalkeeper.Top -= 5;
+                        }
+                        else
+                        {
+                            goalkeeper.Location = new Point(428, 163);
+                            goalkeeperTimer.Enabled = false;
+                            goalkeeper.Image = Resources.goalkeeper;
+                        }
+                        break;
+                    }
+                case "top":
+                    {
+                        goalkeeper.Image = Resources.top_save;
+                        if (goalkeeper.Top > top.Top)
+                        {
+                            goalkeeper.Top -= 10;
+                        }
+                        else
+                        {
+                            goalkeeper.Location = new Point(428, 163);
+                            goalkeeperTimer.Enabled = false;
+                            goalkeeper.Image = Resources.goalkeeper;
+                        }
+                        break;
+                    }
+                case "topright":
+                    {
+                        goalkeeper.Image = Resources.top_right_save;
+                        if (goalkeeper.Left < topRight.Left && goalkeeper.Top > topRight.Top)
+                        {
+                            goalkeeper.Top -= 33;
+                            goalkeeper.Left += 20;
+                        }
+                        else
+                        {
+                            ball.Location = new Point(428, 163);
+                            ballTimer.Enabled = false;
+                            goalkeeper.Image = Resources.goalkeeper;
+                        }
+                        break;
+                    }
+                case "bottomleft":
+                    {
+                        goalkeeper.Top = 203;
+                        goalkeeper.Image = Resources.bottom_left_save;
+                        if (goalkeeper.Left > bottomLeft.Left)
+                        {
+                            goalkeeper.Left -= 20;
+                        }
+                        else
+                        {
+                            goalkeeper.Location = new Point(428, 163);
+                            goalkeeperTimer.Enabled = false;
+                            goalkeeper.Image = Resources.goalkeeper;
+                        }
+                        break;
+                    }
+                case "bottomright":
+                    {
+                        goalkeeper.Top = 203;
+                        goalkeeper.Image = Resources.bottom_right_save;
+                        if (goalkeeper.Left < bottomRight.Left)
+                        {
+                            goalkeeper.Left += 20;
+                        }
+                        else
+                        {
+                            goalkeeper.Location = new Point(428, 163);
+                            goalkeeperTimer.Enabled = false;
+                            goalkeeper.Image = Resources.goalkeeper;
+                        }
+                        break;
+                    }
+            }
         }
 
         private void shootBall(object sender, EventArgs e)
@@ -137,9 +222,32 @@ namespace PenaltyShootoutGame
 
         private void chooseTarget(object sender, EventArgs e)
         {
+            List<string> targets = new List<string>
+            {
+                topLeft.Tag.ToString(),
+                top.Tag.ToString(),
+                topRight.Tag.ToString(),
+                bottomLeft.Tag.ToString(),
+                bottomRight.Tag.ToString(),
+            };
+
+            Random r = new Random();
+            int index = r.Next(targets.Count);
+            goalkeeperTarget = targets[index];
+
             var senderObject = (PictureBox)sender;
             selectedTarget = senderObject.Tag;
             ballTimer.Enabled = true;
+            goalkeeperTimer.Enabled = true;
+
+            if (selectedTarget.ToString() != goalkeeperTarget)
+            {
+                goalsCount.Text = (int.Parse(goalsCount.Text) + 1).ToString();
+            }
+            else
+            {
+                missesCount.Text = (int.Parse(missesCount.Text) + 1).ToString();
+            }
         }
     }
 }
